645a01a23e9c3fcf29dea4d364bc21ddccad7feb
DuongQuyen1309 <minhquyen130904@gmail.com>
2024-10-05 11:31:52 +0700

    login+web
    tạo 1 phần login và web

---
+++ ProjectPRJ/src/java/controller/auth/LoginController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author Duong Minh Quyen
+ */
+public class LoginController extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String param_user = req.getParameter("username");//user input
+        String param_pass = req.getParameter("password");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.html").forward(req, resp);
+    }
+    
+}
